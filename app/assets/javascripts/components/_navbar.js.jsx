8ca3226b2df6ce5b4992295458c50326cc0cf021
Majid <mohammadmajid55@gmail.com>
2019-05-25 19:27:36 +0000

    nav bar is horizontal on top of the page

--- app/assets/javascripts/components/_navbar.js.jsx
+++ app/assets/javascripts/components/_navbar.js.jsx
@@ -1,27 +1,23 @@
 const Navbar = (props) => {
     return(
-        <div class='navigation'>
-            <ul>
+        <div >
+            <ul class='navigation'>
+               
                 <li>Schoober</li>
                 <li>Community Guidelines</li>
                 <li>Profile</li>
-                <li><div class="dropdown">
-                    <button class="dropbtn">Trip Planner</button>
 
+                <li>
+                    <div class="dropdown">
+                        <button class="dropbtn">Trip Planner</button>
                         <div class="dropdown-content">
                             <a href="#">Parent Trip Planner</a>
                             <a href="#">Driver Trip Planner</a>
-    
                         </div>
                     </div> 
-                    
                 </li>
                 <li>Dashboard</li>
                 <li>Logout</li>
-            
-
-
-
             </ul>
         </div>
     )
