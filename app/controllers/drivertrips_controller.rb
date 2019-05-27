d26ae66a3d125264fce52717f3b89bd59d0718ab
Soojeong Choe <soojeong8@gmail.com>
2019-05-27 13:30:15 -0400

    created driver trip form and redirects to dashboard

--- app/controllers/drivertrips_controller.rb
+++ app/controllers/drivertrips_controller.rb
@@ -1,11 +1,16 @@
 class DrivertripsController < ApplicationController
   def create
     drivertrip = DriverTrip.new(drivertrip_params)
+    puts "session user id" 
+    puts session[:user_id]
+    user = User.find_by_id session[:user_id]
+    puts "params inspect"
+    puts params.inspect
 
     if drivertrip.save!
 
       flash[:success] = "You have successfully created a new project!"
-      redirect_to @drivertrip
+      redirect_to user
     else
       redirect_to '/drivertrips/new'
     end
@@ -22,7 +27,7 @@ class DrivertripsController < ApplicationController
 private
 
 def drivertrip_params
-    params.require(:drivertrip).permit(:start_point, :end_point, :trip_date, :time_slot, :spots_available, :user_id)
+    params.require(:drivertrip).permit(:start_point, :end_point, :trip_date, :time_slot, :user_id, :spots_available)
 end
 
 end
