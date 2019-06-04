ed577f81edc73fdc2551279a1113946a42f07847
Soojeong Choe <soojeong8@gmail.com>
2019-06-04 12:39:54 -0400

    Name appears on navbar dashboardtab

--- app/assets/javascripts/components/_navbar.js.jsx
+++ app/assets/javascripts/components/_navbar.js.jsx
@@ -45,7 +45,7 @@ const Navbar = ({current_user}) => {
             </div>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href={`/users/${current_user.id}`}>Dashboard</a>
+            <a className="nav-link" href={`/users/${current_user.id}`}>{`${current_user.first_name}`}'s Dashboard</a>
           </li>
           
           <li className="nav-item">
