fb4e3f6a22524457955d90d10069ac1e624bb94a
Soojeong Choe <soojeong8@gmail.com>
2019-06-05 00:22:56 -0400

    flash message when address input is wrong

--- app/controllers/parenttrips_controller.rb
+++ app/controllers/parenttrips_controller.rb
@@ -77,146 +77,159 @@ class ParenttripsController < ApplicationController
     @index = 0
 
     parent_startpoint = Geocoder.search(@parenttrips.start_point)
-    parent_coordinates = parent_startpoint.first.coordinates
 
+    puts "parent_startpoint errorrrrrrrrr"
+    puts parent_startpoint.inspect
 
-    @drivertrips = DriverTrip.where(end_point: @parenttrips.end_point, time_slot: @parenttrips.time_slot, trip_date: @parenttrips.trip_date)
-    
-    puts "@drivertrips areeeeeeee"
-    puts @drivertrips.inspect
-
-    index = 0
-    @drivertrips.each do |drivertrip| 
-      index += 1
-      puts 'index is ................'
-      puts index
-    
-      # change address from driver trips point A to long and lat
-      driver_startpoint = Geocoder.search(drivertrip.start_point)
+    if parent_startpoint == []
+      puts "pplease hiitttt"
+      flash[:alert] = "Wrong Address Inputed"
+      redirect_to '/parenttrips/new' 
       
-      driver_coordinates = driver_startpoint.first.coordinates
-
-      driver_startlocation = {
-        :lat => driver_coordinates[0],
-        :lng => driver_coordinates[1],
-        :infowindow=> "<strong>Driver: #{drivertrip.user_id}</strong>" + 
-        "<div>Address: #{drivertrip.start_point}</div>" +
-        "<div>Date: #{drivertrip.trip_date}</div>" + 
-        "<div>Time: #{drivertrip.time_slot}</div>" + 
-        "<div>Spots Reserved: #{drivertrip.spots_available}</div>",
-        :radius => 209.344,
-        :strokeColor => "#0000FF",
-        :fillColor => "#0000FF"
-      }
-
-      driverTripCoordinates.push(driver_startlocation)
-
-      puts 'distance between driver startpoint and parent startpoint'
-      puts Geocoder::Calculations.distance_between(driver_coordinates, parent_coordinates)
-
-      distanceBetweenStartPoints = Geocoder::Calculations.distance_between(driver_coordinates, parent_coordinates)
-      # if point A of driver trip and point A of parent trip is close enough, then put them into filter trips array
-      if distanceBetweenStartPoints < 5
-          selectedDriverTrips.push(drivertrip)
-      end
-    # this end closes the loop
-    end
+    else
+           
+       
+        parent_coordinates = parent_startpoint.first.coordinates
 
 
+        @drivertrips = DriverTrip.where(end_point: @parenttrips.end_point, time_slot: @parenttrips.time_slot, trip_date: @parenttrips.trip_date)
+        
+        puts "@drivertrips areeeeeeee"
+        puts @drivertrips.inspect
 
-    # from the filter array, display into show page
-    @selectedDriverTrips = selectedDriverTrips
-    @drivercoordinates = driverTripCoordinates
+        index = 0
+        @drivertrips.each do |drivertrip| 
+          index += 1
+          puts 'index is ................'
+          puts index
+        
+          # change address from driver trips point A to long and lat
+          driver_startpoint = Geocoder.search(drivertrip.start_point)
+          
+          driver_coordinates = driver_startpoint.first.coordinates
 
-    puts 'drivertripcoordinates are .........'
-    puts driverTripCoordinates
+          driver_startlocation = {
+            :lat => driver_coordinates[0],
+            :lng => driver_coordinates[1],
+            :infowindow=> "<strong>Driver: #{drivertrip.user_id}</strong>" + 
+            "<div>Address: #{drivertrip.start_point}</div>" +
+            "<div>Date: #{drivertrip.trip_date}</div>" + 
+            "<div>Time: #{drivertrip.time_slot}</div>" + 
+            "<div>Spots Reserved: #{drivertrip.spots_available}</div>",
+            :radius => 209.344,
+            :strokeColor => "#0000FF",
+            :fillColor => "#0000FF"
+          }
 
-  
+          driverTripCoordinates.push(driver_startlocation)
 
+          puts 'distance between driver startpoint and parent startpoint'
+          puts Geocoder::Calculations.distance_between(driver_coordinates, parent_coordinates)
 
-
-    @parent_startlocation = {
-      :lat=>parent_coordinates[0],
-      :lng=>parent_coordinates[1],
-      :infowindow=> "<strong>You</strong>" + 
-                    "<div>Address: #{@parenttrips.start_point}</div>" +
-                    "<div>Date: #{@parenttrips.trip_date}</div>" + 
-                    "<div>Time: #{@parenttrips.time_slot}</div>" + 
-                    "<div>Spots Reserved: #{@parenttrips.spots_required}</div>",
-      :radius => 209.344,
-      :strokeColor => "#42f442",
-      :fillColor => "#42f442"
-    }
-    puts 'parent start location is'
-    puts @parent_startlocation
+          distanceBetweenStartPoints = Geocoder::Calculations.distance_between(driver_coordinates, parent_coordinates)
+          # if point A of driver trip and point A of parent trip is close enough, then put them into filter trips array
+          if distanceBetweenStartPoints < 5
+              selectedDriverTrips.push(drivertrip)
+          end
+        # this end closes the loop
+        end
 
 
 
-    school_point = Geocoder.search(@parenttrips.end_point)
-    school_coordinates = school_point.first.coordinates
-    puts "school coordinates are...."
-    puts school_coordinates 
-    puts "@parenttrips.end_point is"
-    puts @parenttrips.end_point
+        # from the filter array, display into show page
+        @selectedDriverTrips = selectedDriverTrips
+        @drivercoordinates = driverTripCoordinates
 
-    @school_location = {
-      :lat=>school_coordinates[0],
-      :lng=>school_coordinates[1],
-      :infowindow=> "<strong>Schoober Designated School</strong>" + 
-                    "<div>Address: #{@parenttrips.end_point}</div>",
-      :radius => 209.344,
-      :strokeColor => "#42f442",
-      :fillColor => "#42f442"
-    }
-    puts 'school location is'
-    puts @school_location 
+        puts 'drivertripcoordinates are .........'
+        puts driverTripCoordinates
 
-   if (selectedDriverTrips == [])
+      
 
-    @school_location1 = {
-      :lat=>43.720370,
-      :lng=>-79.413720,
-      :infowindow=> "<div><strong>Havergal College</strong></div>"+
-                    "<div>Address: 21451 Avenue Rd, North York, ON </div>",
-                  
-                    
-      :radius => 1609.344,
-      :strokeColor => "#f44141",
-      :fillColor => "#f44141"
-    }
-    
-    @school_location2 = {
-      :lat=>43.733002,
-      :lng=>-79.378899,
-      :infowindow=> "<div><strong>Crescent School</strong></div>"+
-                    "<div>Address: 2365 Bayview Ave, North York, ON </div>",                   
-                    
-      :radius => 1609.344,
-      :strokeColor => "#f44141",
-      :fillColor => "#f44141"
-    }
 
-    @school_location3 = {
-      :lat=>43.690650,
-      :lng=>-79.404760,
-      :infowindow=> "<div><strong>Upper Canada College</strong></div>"+
-                    "<div>Address: 220 Lonsdale Rd, Toronto, ON </div>",
-                    
-      :radius => 1609.344,
-      :strokeColor => "#f44141",
-      :fillColor => "#f44141"
-    }
-    
-    @school_location4 = {
-      :lat=>43.666570,
-      :lng=>-79.402510,
-      :infowindow=> "<div><strong>University of Toronto Schools</strong></div>"+
-                    "<div>Address: 371 Bloor St W, Toronto, ON </div>",
-                    
-      :radius => 1609.344,
-      :strokeColor => "#f44141",
-      :fillColor => "#f44141"
-    }
+
+        @parent_startlocation = {
+          :lat=>parent_coordinates[0],
+          :lng=>parent_coordinates[1],
+          :infowindow=> "<strong>You</strong>" + 
+                        "<div>Address: #{@parenttrips.start_point}</div>" +
+                        "<div>Date: #{@parenttrips.trip_date}</div>" + 
+                        "<div>Time: #{@parenttrips.time_slot}</div>" + 
+                        "<div>Spots Reserved: #{@parenttrips.spots_required}</div>",
+          :radius => 209.344,
+          :strokeColor => "#42f442",
+          :fillColor => "#42f442"
+        }
+        puts 'parent start location is'
+        puts @parent_startlocation
+
+
+
+        school_point = Geocoder.search(@parenttrips.end_point)
+        school_coordinates = school_point.first.coordinates
+        puts "school coordinates are...."
+        puts school_coordinates 
+        puts "@parenttrips.end_point is"
+        puts @parenttrips.end_point
+
+        @school_location = {
+          :lat=>school_coordinates[0],
+          :lng=>school_coordinates[1],
+          :infowindow=> "<strong>Schoober Designated School</strong>" + 
+                        "<div>Address: #{@parenttrips.end_point}</div>",
+          :radius => 209.344,
+          :strokeColor => "#42f442",
+          :fillColor => "#42f442"
+        }
+        puts 'school location is'
+        puts @school_location 
+
+      if (selectedDriverTrips == [])
+
+        @school_location1 = {
+          :lat=>43.720370,
+          :lng=>-79.413720,
+          :infowindow=> "<div><strong>Havergal College</strong></div>"+
+                        "<div>Address: 21451 Avenue Rd, North York, ON </div>",
+                      
+                        
+          :radius => 1609.344,
+          :strokeColor => "#f44141",
+          :fillColor => "#f44141"
+        }
+        
+        @school_location2 = {
+          :lat=>43.733002,
+          :lng=>-79.378899,
+          :infowindow=> "<div><strong>Crescent School</strong></div>"+
+                        "<div>Address: 2365 Bayview Ave, North York, ON </div>",                   
+                        
+          :radius => 1609.344,
+          :strokeColor => "#f44141",
+          :fillColor => "#f44141"
+        }
+
+        @school_location3 = {
+          :lat=>43.690650,
+          :lng=>-79.404760,
+          :infowindow=> "<div><strong>Upper Canada College</strong></div>"+
+                        "<div>Address: 220 Lonsdale Rd, Toronto, ON </div>",
+                        
+          :radius => 1609.344,
+          :strokeColor => "#f44141",
+          :fillColor => "#f44141"
+        }
+        
+        @school_location4 = {
+          :lat=>43.666570,
+          :lng=>-79.402510,
+          :infowindow=> "<div><strong>University of Toronto Schools</strong></div>"+
+                        "<div>Address: 371 Bloor St W, Toronto, ON </div>",
+                        
+          :radius => 1609.344,
+          :strokeColor => "#f44141",
+          :fillColor => "#f44141"
+        }
+        
    end
 
    @rosedale =
@@ -289,16 +302,10 @@ class ParenttripsController < ApplicationController
       :lng=>-79.388827,
       :strokeColor => "#f2968a",
       :fillColor => "#f2968a"
-    }
-
-
-  ]
-
-
-
-
+    }]
   end
-    
+end
+
 
   def parenttrip_params
       params.require(:parenttrip).permit(:start_point, :end_point, :trip_date, :time_slot, :user_id, :spots_required)
