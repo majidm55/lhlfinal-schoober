17a810d57302881ff2be462be80af9b92f949be3
Soojeong Choe <soojeong8@gmail.com>
2019-06-10 13:21:11 -0400

    Got rid of User line since its not being used

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,5 @@
 class ProfilesController < ApplicationController
   def index
-    # @user = User.find params[:id]
     @profile_id = session[:user_id]
     @user = User.find_by_id session[:user_id]
   end
