63cb5144789e5893b999c6d4037935f63f0cd427
Soojeong Choe <soojeong8@gmail.com>
2019-06-10 12:55:31 -0400

    Cleaned up profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,12 +3,5 @@ class ProfilesController < ApplicationController
     # @user = User.find params[:id]
     @profile_id = session[:user_id]
     @user = User.find_by_id session[:user_id]
-
-
-
   end
-
-
-
-
 end
